8a209a0e3db5b4224f7b1cd2ee24c3465afaf95c
brody <brody.bassett@pacificfusion.com>
2025-09-25 17:14:28 +0000

    Missing machine

--- ats/configuration.py
+++ ats/configuration.py
@@ -110,7 +110,7 @@ def addOptions(parser):
         add_flux_only_options(parser)
 
     # Toss specific options (slurm)
-    elif SYS_TYPE.startswith('toss') or SYS_TYPE.startswith('hpc6a'):
+    elif SYS_TYPE.startswith('toss') or SYS_TYPE.startswith('hpc'):
         add_toss3_only_options(parser)
 
     # Sierra Specific options (lsf)
